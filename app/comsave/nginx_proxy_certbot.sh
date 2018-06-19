42c0ffaeb9b804a0737ce223657cd4627ae3dd89
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2018-06-19 17:27:29 +0200

    add missing LETSENCRYPT_EMAIL

--- app/comsave/nginx_proxy_certbot.sh
+++ app/comsave/nginx_proxy_certbot.sh
@@ -7,7 +7,7 @@ for container_id in $(curl -s -X GET --unix-socket /tmp/docker.sock http://v1.37
   if [ "$container_virtual_host" != "null" ]; then
     container_virtual_host=$(eval echo "$container_virtual_host" | sed 's/VIRTUAL_HOST=\(.*\)/\1/')
 
-    certbot certonly --nginx --agree-tos -n -m joeri.veder@comsave.com -d $container_virtual_host --expand 2>/var/log/certbot.log
+    certbot certonly --nginx --agree-tos -n -m $LETSENCRYPT_EMAIL -d $container_virtual_host --expand 2>/var/log/certbot.log
 
     ln -s /etc/letsencrypt/live/$container_virtual_host/fullchain.pem /etc/nginx/certs/$container_virtual_host.crt
     ln -s /etc/letsencrypt/live/$container_virtual_host/privkey.pem /etc/nginx/certs/$container_virtual_host.key
