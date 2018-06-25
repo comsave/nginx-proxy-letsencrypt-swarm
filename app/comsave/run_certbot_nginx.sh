3fee3969a63e25b4fe3e2c23f25ef9e8bd184169
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2018-06-25 16:18:08 +0200

    add multihost support

--- app/comsave/run_certbot_nginx.sh
+++ app/comsave/run_certbot_nginx.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-SERVICE_VIRTUAL_HOSTS=$(curl -s -X GET --unix-socket /tmp/docker.sock "http://v1.37/services" | jq '.[].Spec.TaskTemplate.ContainerSpec.Env  | map(select(. | contains("VIRTUAL_HOST")))[0]')
+SERVICES_VIRTUAL_HOST_ENVS=$(curl -s -X GET --unix-socket /tmp/docker.sock "http://v1.37/services" | jq '.[].Spec.TaskTemplate.ContainerSpec.Env  | map(select(. | contains("VIRTUAL_HOST")))[0]')
 
-for SERVICE_VIRTUAL_HOST in $SERVICES_VIRTUAL_HOST; do
-  if [ ! -z "$SERVICE_VIRTUAL_HOST" ]; then
-    SERVICE_VIRTUAL_HOST=$(eval echo "$SERVICE_VIRTUAL_HOST" | sed 's/VIRTUAL_HOST=\(.*\)/\1/')
+for SERVICE_VIRTUAL_HOST_ENV in $SERVICES_VIRTUAL_HOST_ENVS; do
+  if [ ! -z "$SERVICE_VIRTUAL_HOST_ENV" ]; then
+    SERVICE_VIRTUAL_HOSTS=$(eval echo "$SERVICE_VIRTUAL_HOST_ENV" | sed 's/VIRTUAL_HOST=\(.*\)/\1/')
 
-    if [ ! -d /etc/letsencrypt/live/$SERVICE_VIRTUAL_HOST ]; then
-      certbot certonly --nginx --agree-tos -n -m $LETSENCRYPT_EMAIL -d $SERVICE_VIRTUAL_HOST --expand 2>/var/log/certbot.log || true
-    fi
+    for SERVICE_VIRTUAL_HOSTS in $(echo ${IN//,/ }); do
+      if [ ! -d /etc/letsencrypt/live/$SERVICE_VIRTUAL_HOST ]; then
+        certbot certonly --nginx --agree-tos -n -m $LETSENCRYPT_EMAIL -d $SERVICE_VIRTUAL_HOST --expand 2>/var/log/certbot.log || true
+      fi
 
-    /app/comsave/symlink_nginx_cert.sh $SERVICE_VIRTUAL_HOST
+      /app/comsave/symlink_nginx_cert.sh $SERVICE_VIRTUAL_HOST
+    done
   fi
 done
