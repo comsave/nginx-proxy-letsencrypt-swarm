5f7dbd5537f4a712fa54e621274bbc2d460034d3
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2018-06-25 13:55:57 +0200

    move nginx reload to docker-gen notify

--- app/comsave/after_dockergen.sh
+++ app/comsave/after_dockergen.sh
@@ -3,8 +3,7 @@ echo "Running /app/comsave/after_dockergen.sh ..."
 
 (
  /app/comsave/run_certbot_nginx.sh \
- && docker-gen /app/nginx.tmpl /etc/nginx/conf.d/default.conf \
- && nginx -s reload \
+ && docker-gen -notify="nginx -s reload" /app/nginx.tmpl /etc/nginx/conf.d/default.conf \
  && /app/comsave/sync_push_certs.sh
 ) &
 
--- app/comsave/entrypoint.sh
+++ app/comsave/entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 /app/comsave/setup_sync.sh \
  && rm -rf /etc/letsencrypt/* \
  && /app/comsave/sync_pull_certs.sh \
- && /app/comsave/symlink_synced_certs.sh \
-
-docker-gen /app/nginx.tmpl /etc/nginx/conf.d/default.conf
+ && /app/comsave/symlink_synced_certs.sh
 
 /app/comsave/setup_certbot.sh
+
+docker-gen -notify="nginx -s reload" /app/nginx.tmpl /etc/nginx/conf.d/default.conf
 
 /app/docker-entrypoint.sh
 
