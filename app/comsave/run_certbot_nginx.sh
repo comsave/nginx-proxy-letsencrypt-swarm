e1842dc43549615d268f9154b2fbd88891dc9c06
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2018-06-27 16:58:35 +0200

    recover letsencrypt live folder check

--- app/comsave/run_certbot_nginx.sh
+++ app/comsave/run_certbot_nginx.sh
@@ -7,9 +7,9 @@ for SERVICE_VIRTUAL_HOST_ENV in $SERVICES_VIRTUAL_HOST_ENVS; do
     SERVICE_VIRTUAL_HOSTS=$(eval echo "$SERVICE_VIRTUAL_HOST_ENV" | sed 's/VIRTUAL_HOST=\(.*\)/\1/')
 
     for SERVICE_VIRTUAL_HOSTS in $(echo ${IN//,/ }); do
-      # if [ ! -d /etc/letsencrypt/live/$SERVICE_VIRTUAL_HOST ]; then
-      certbot certonly --nginx --agree-tos -n -m $LETSENCRYPT_EMAIL -d $SERVICE_VIRTUAL_HOST --expand 2>/var/log/certbot.log || true
-      # fi
+      if [ ! -d /etc/letsencrypt/live/$SERVICE_VIRTUAL_HOST ]; then
+        certbot certonly --nginx --agree-tos -n -m $LETSENCRYPT_EMAIL -d $SERVICE_VIRTUAL_HOST --expand 2>/var/log/certbot.log || true
+      fi
 
       /app/comsave/symlink_nginx_cert.sh $SERVICE_VIRTUAL_HOST
     done
